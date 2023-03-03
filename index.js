85370c7bb5b73cf2c4836432965c38ae94316b6e
RajkumarGajbhiye <109793318+RajkumarGajbhiye@users.noreply.github.com>
2023-03-03 17:24:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Connected to the server!");
 });
+app.get("/friends", (req, res) => {
+  res.send("Hellow friends!");
+});
+
 app.listen(3000, () => console.log("Running in 3k..."));
 module.exports = app
